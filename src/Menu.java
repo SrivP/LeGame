440d2e905a43bf355662c6434f7734fb4b677780
owopi <awesomebuilds.playmc@gmail.com>
2022-12-06 21:15:05 -0500

    made stuff work

--- src/Menu.java
+++ src/Menu.java
@@ -5,8 +5,8 @@ import java.util.EventListener;
 
 public class Menu extends JPanel implements EventListener {
     ImageIcon bg = new ImageIcon("DoJo.gif");
-    private JButton b1, b2, b3;     //declare 3 buttons
-    Rectangle frameSize;
+    private JButton b1, b2, b3;
+
     JLabel l;
 
     public Menu(Dimension r) {
@@ -43,38 +43,6 @@ public class Menu extends JPanel implements EventListener {
     }
 
 }
-class MyGamePanel_withBox extends JPanel {
-    private JButton b1, b2, b3;    //declare 3 buttons
-    private JTextField txt1, txt2, txt3;   //declare 3 Test Fields
 
-    public MyGamePanel_withBox() {    // constructor
-        b1= new JButton("some button");
-        b2= new JButton("one more button");
-        b3= new JButton("third button");
-
-        txt1= new JTextField("not empty txt");
-        txt2= new JTextField(20);  // the size is 20 chars
-        txt3= new JTextField("enter something");
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(Box.createRigidArea(new Dimension(0, 65)));// space at the top 0 -> with - we don't care, 65 ->height
-
-        this.add(txt1);
-        this.add(Box.createRigidArea(new Dimension(10, 45)));// space between components
-
-        b1.setAlignmentX(Component.CENTER_ALIGNMENT);	 // set horizontal allignment to center
-        this.add(b1);
-        this.add(Box.createRigidArea(new Dimension(0, 45)));// space between components
-
-        b2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(b2);
-        this.add(Box.createRigidArea(new Dimension(0, 45)));// space between components
-
-        b3.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(b3);
-        this.add(Box.createRigidArea(new Dimension(10, 135)));// space at the end
-
-    }
-}
 
 
