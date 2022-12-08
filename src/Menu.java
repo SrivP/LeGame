0033c8e558a85ec85ac18a621752e5b16354001b
surya <tallapaneni.surya@gmail.com>
2022-12-08 14:30:55 -0500

    fixed bugs and optimized code

--- src/Menu.java
+++ src/Menu.java
@@ -5,40 +5,30 @@ import java.awt.event.ActionListener;
 
 public class Menu extends JPanel implements ActionListener {
     private ImageIcon bg = new ImageIcon("dojo1.gif");
-    private JButton b1, b2, b3;
+    private JButton[] b = new JButton[3];  //declare 3 buttons
 
     public Menu() {
 
-        b1= new JButton("Play");
-        b1.addActionListener(this);
-        b2= new JButton("Instructions");
-        b3= new JButton("Exit");
+        b[0] = new JButton("Play");
+        b[0].addActionListener(this);
+        b[1] = new JButton("Instructions");
+        b[2] = new JButton("Exit");
 
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(Box.createRigidArea(new Dimension(0, 65)));// space at the top 0 -> with - we don't care, 65 ->height
+        this.setLayout(new GridLayout(3, 0, 30, 30));
 
-
-        b1.setAlignmentX(Component.CENTER_ALIGNMENT);	 // set horizontal allignment to center
-        this.add(b1);
-        this.add(Box.createRigidArea(new Dimension(0, 120 )));// space between components
-
-        b2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(b2);
-        this.add(Box.createRigidArea(new Dimension(0, 120)));// space between components
-
-        b3.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.add(b3);
-        this.add(Box.createRigidArea(new Dimension(10, 120 )));// space at the end
+        for (int i = 0; i < b.length; i++) {
+            b[i].setAlignmentY(Component.CENTER_ALIGNMENT);
+            this.add(b[i]);
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawRect(20,100,100,100);
         bg.paintIcon(this, g, 0, 0);
 
     }
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == b1)
+        if (e.getSource() == b[0])
             Main.cdly.next(Main.c);
     }
 
@@ -46,7 +36,7 @@ public class Menu extends JPanel implements ActionListener {
 }
 
 class Levels extends JPanel implements ActionListener {
-    private JButton[] b = new JButton[10];  //declare 3 buttons
+    private JButton[] b = new JButton[4];  //declare 3 buttons
     private ImageIcon bg = new ImageIcon("dungeon.gif");
 
 
