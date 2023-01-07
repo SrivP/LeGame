023387fe534abac59680c3ec27ba2a996395d7aa
owopi <awesomebuilds.playmc@gmail.com>
2023-01-06 21:52:10 -0500

    feat: added experimental code (lvl 2 of the game) in SpriteGame.java, has a display bug

--- src/SpriteGame.java
+++ src/SpriteGame.java
@@ -302,7 +302,6 @@ class Enemy extends Sprite {
 }
 
 
-
 // The Projectile class represents a projectile fired by the player sprite.
 // It has a position, size, and velocity, and can be either active or inactive.
 class Projectile {
